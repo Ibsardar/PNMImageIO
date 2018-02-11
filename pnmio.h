ae14cab35a2ed1b4c12c7604a524e1410ce4604a
Ibrahim Sardar <ibrysar@gmail.com>
2018-02-11 04:34:48 -0500

    tweakin'

--- pnmio.h
+++ pnmio.h
@@ -10,7 +10,7 @@
 //
 //  Honor Pledge:
 //
-//  I pledge that I have neither given nor received any help on this assignment.
+//  I pledge that I have neither given nor received any help on this project.
 //
 //  ibsardar
 //
